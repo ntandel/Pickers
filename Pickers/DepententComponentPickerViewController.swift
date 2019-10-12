3b58ab002242b211831d1d40247e78d62522b2dd
Nisha Tandel <angeltandel64.nt@gmail.com>
2019-10-12 02:30:20 -0400

    Picker classes created Date Single component DoubleComponent DependentComponent Custom

---
+++ Pickers/DepententComponentPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DepententComponentPickerViewController.swift
+//  Pickers
+//
+//  Created by Nisha Tandel on 2019-10-12.
+//  Copyright © 2019 Nisha Tandel. All rights reserved.
+//
+
+import UIKit
+
+class DepententComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pickers/SingleComponentPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingleComponentPickerViewController.swift
+//  Pickers
+//
+//  Created by Nisha Tandel on 2019-10-12.
+//  Copyright © 2019 Nisha Tandel. All rights reserved.
+//
+
+import UIKit
+
+class SingleComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
